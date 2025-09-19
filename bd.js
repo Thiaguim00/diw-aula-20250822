6363a1c6df18572c0eb1a8b2caa61d3de73878b6
Thiaguim <thiagohenque10050@gmail.com>
2025-09-19 23:49:17 +0000

    att

---
+++ bd.js
@@ -0,0 +1,38 @@
+const usuario = {
+    "email": "admin@email.com",
+    "senha": "admin"
+};
+/*
+const dados = [
+    {
+        "id":,
+        "titulo": ,
+        "descrição": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "assets/imagem/"
+    };
+{
+    "id":,
+    "titulo": ,
+    "descrição": ,
+    "conteudo": ,
+    "categoria": ,
+    "autor": ,
+    "data": ,
+    "imagem": "assets/imagem/"
+},
+{
+    "id":,
+    "titulo": ,
+    "descrição": ,
+    "conteudo": ,
+    "categoria": ,
+    "autor": ,
+    "data": ,
+    "imagem": "assets/imagem/"
+},
+];
+*/
---
+++ login.js
@@ -0,0 +1,19 @@
+const email = usuario.email;
+const senha = usuario.senha;
+
+function verificarCredenciais() {
+
+    const emailInformado = document.getElementById("email").value;
+    const senhaInformada = document.getElementById("senha").value;
+
+    if (emailInformado === email) {
+        alert("E-mail informado corretamente!");
+        if (senhaInformada === senha) {
+            alert("Senha Informada corretamente");
+            window.location = "home.html";
+        } else
+            alert("Senha incorreta!");
+    }
+    else
+        alert("E-mail informado incorretamente!");
+}
